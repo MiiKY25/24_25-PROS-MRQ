22dba6727411335ce026beaffde65ac14abf7cc2
mikel <mikel.ra.2003@gmail.com>
2024-10-15 10:52:15 +0200

    UD1.ACT11.intento1.definitivo

---
+++ UD1/ACT11/Actividad11.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Actividad11 {
+    public static void main(String[] args) {
+        Runtime r = Runtime.getRuntime();
+        String comando = "java Ejemplo2"; // Comando para ejecutar Ejemplo2
+        Process p = null;
+
+        try {
+            p = r.exec(comando);
+            InputStream is = p.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String linea;
+            while((linea = br.readLine()) != null) { // lee una línea
+                System.out.println(linea);
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Comprobación: Valor=0 bien, Valor=-1 mal
+        int exitVal;
+        try {
+            exitVal = p.waitFor();
+            System.out.println("Valor de Salida " + exitVal);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
