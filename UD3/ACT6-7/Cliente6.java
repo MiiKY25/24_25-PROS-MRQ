05af2d0ba0edc51657c095986e6a9ef1888eb96f
MiiKY25 <117351410+MiiKY25@users.noreply.github.com>
2024-12-03 11:45:58 +0100

    UD3.ACT6-7.intento1.inicial

---
+++ UD3/ACT6-7/Cliente6.java
@@ -0,0 +1,58 @@
+package Actividad6_7;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Cliente6 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String host="localhost";
+
+		int puerto = 6011;
+
+		System.out.println("PROGRAMA CLIENTE INICIANDO");
+		
+		//Leer numero de la consola
+		System.out.println("Introduce un número");
+		int num = Integer.parseInt(scanner.nextLine());
+		
+		try {
+			Socket Cliente = new Socket (host, puerto);
+			
+			// Flujo de salida al servidor para poder enviar informacion 
+			DataOutputStream flujoSalida = new DataOutputStream (Cliente.getOutputStream());
+
+			//Enviar el numero al servidor
+			flujoSalida.writeUTF(num+"");
+			
+			// Creación del flujo de entrada del servidor
+			InputStream entrada = null;
+			entrada = Cliente.getInputStream();
+			DataInputStream flujoEntrada = new DataInputStream (entrada);
+		
+			//Leer el numero que ha enviado el cliente
+			String mensaje=flujoEntrada.readUTF();
+			System.out.println("Recibiendo mensaje del servidor: ");
+			System.out.println(mensaje);
+			
+			
+			
+			// Cerrar streams y sockets
+			flujoSalida.close();
+			Cliente.close();
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ UD3/ACT6-7/Servidor6.java
@@ -0,0 +1,55 @@
+package Actividad6_7;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Servidor6 {
+	
+	public static void main(String[] args) {
+		
+		int puerto=6011;
+		
+		try {
+			ServerSocket servidor = new ServerSocket (puerto);
+
+			Socket clienteConectado = null;
+
+			System.out.println("Esperando al cliente.....");
+
+			clienteConectado = servidor.accept();
+			
+			// Creación del flujo de entrada del cliente
+			InputStream entrada = null;
+			entrada = clienteConectado.getInputStream();
+			DataInputStream flujoEntrada = new DataInputStream (entrada);
+		
+			//Leer el numero que ha enviado el cliente
+			int num=Integer.parseInt(flujoEntrada.readUTF());
+			
+			int cuadrado=num*num;
+			
+			// Flujo de salida al servidor
+			DataOutputStream flujoSalida = new DataOutputStream (clienteConectado.getOutputStream());
+
+			// Enviar el cuadradro al cliente 
+			flujoSalida.writeUTF("\tEl cuadrado del número "+num+" es "+cuadrado);
+
+			// Cerrar streams y sockets
+			entrada.close();
+			flujoEntrada.close();
+			clienteConectado.close();
+			servidor.close(); 
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
